bda6224ceea1a997b7f53f3812605d659e2619fb
Marco Silva <m.samcro@hotmail.com>
2024-03-16 00:40:02 -0300

    cambio error baseurl

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments={
-  baseUrl: 'apisistemaventa.somee.com/api',
+  baseUrl: 'https://apisistemaventa.somee.com/api',
   imgurClientId: 'b08dba61115166adb3202e2435aeb494',
   imgUploadUrl: 'https://api.imgur.com/3/image',
   production: true
