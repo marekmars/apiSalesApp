28f416d09932c40b7fb97890ba4f953fa3c8f627
Marco Silva <m.samcro@hotmail.com>
2024-03-16 10:22:48 -0300

    modificacion nueva en endpoints de la api

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environments={
-  baseUrl: 'https://apisistemaventa.somee.com/api',
+  baseUrl: 'https://sales-api.somee.com/api',
   imgurClientId: 'b08dba61115166adb3202e2435aeb494',
   imgUploadUrl: 'https://api.imgur.com/3/image',
   production: true
